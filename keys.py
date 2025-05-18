801b6e3bd8f3a02fb13f48cc43ab34c605f6b9c8
thama1997 <thama1997@yahoo.com>
2025-05-18 15:26:20 +0200

    initial

---
+++ keys.py
@@ -0,0 +1,12 @@
+import pygame
+
+vk_right = pygame.K_RIGHT
+vk_left = pygame.K_LEFT
+vk_up = pygame.K_UP
+vk_down = pygame.K_DOWN
+vk_enter = pygame.K_RETURN
+vk_escape = pygame.K_ESCAPE
+
+def ord(key):
+    if key == "w":
+       pygame.K_w
